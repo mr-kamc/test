9ac8b3469db7651133b383f280de1c5cc580e0b1
Алексей <mr.kamc@gmail.com>
2016-07-04 15:35:41 +0300

    сырой каркас игры в города

--- city.php
+++ city.php
@@ -2,34 +2,39 @@
 
 $cities = include __DIR__ . '/data.php';
 
-$us = $_POST;
+findCity($cities,$_POST);
 
-$res = array_intersect($us,$cities);
+function findCity($cities,$us)
+{
+    //$us = $_POST;
+
+    $res = array_intersect($us,$cities);
 
 //определение последнего символа
-$last =substr($res['city'],-2);
-$first = substr($res['city'],0,2);
+    $last =substr($res['city'],-2);
+    $first = substr($res['city'],0,2);
 
-if($res){
-    echo 'есть город';
-} else {
-    echo 'нет города';
-}
-
-var_dump($res);
-var_dump($first);
-
-$fin = [];
-foreach($cities as $key=>$cit){
-    $result = [];
-
-    $citLow = mb_strtolower(substr($cit,0,2));//города в нижнем регистре
-
-    if($last==$citLow){
-        echo $cit;
-        break;
+    if($res){
+        echo 'есть город';
+    } else {
+        echo 'нет города';
     }
 
+    var_dump($res);
+    var_dump($first);
+
+    $fin = [];
+    foreach($cities as $key=>$cit){
+        $result = [];
+
+        $citLow = mb_strtolower(substr($cit,0,2));//города в нижнем регистре
+
+        if($last==$citLow){
+            echo $cit;
+            break;
+        }
+
+    }
 }
 
 
